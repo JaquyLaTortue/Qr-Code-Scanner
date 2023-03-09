2fb7e008ed04e669115ccee13b836f9376a9751f
Léo <leolecointre50@gmail.com>
2023-03-09 17:06:28 +0100

    Début de L'UI

---
+++ Assets/Scripts/Enterprise.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enterprise : MonoBehaviour
+{
+    public new string name;
+    public string size;
+    public int value;
+    public bool hasIncrease;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
