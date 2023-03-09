cf3fd21ba45a8f3f9d00ce5097a049a0fc1515be
JaquyLaTortue <93046036+JaquyLaTortue@users.noreply.github.com>
2023-03-09 11:56:45 +0100

    try rotate camera

--- Assets/QrCodeScanner.cs
+++ Assets/QrCodeScanner.cs
@@ -2,25 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+//using zxing;
 
 public class QrCodeScanner : MonoBehaviour
 {
     public WebCamTexture Cam;
     public RawImage CamDisplay;
-
+    public Quaternion baseRotation;
+    public Transform camTr;
 
     // Start is called before the first frame update
     void Start()
     {
-        Cam = new WebCamTexture();
-        //GetComponent<Renderer>().material.mainTexture = Cam;
+        string frontCamName = null;
+        var webCamDevices = WebCamTexture.devices;
+        foreach (var camDevice in webCamDevices)
+        {
+            if (camDevice.isFrontFacing)
+            {
+                frontCamName = camDevice.name;
+                break;
+            }
+        }
+
+        Cam = new WebCamTexture(frontCamName, Screen.width, Screen.height);
         CamDisplay.texture = Cam;
-        Cam.Play();
+        CamDisplay.gameObject.SetActive(false);
+
+        baseRotation = camTr.transform.rotation;
+        camTr.transform.rotation = baseRotation * Quaternion.AngleAxis(Cam.videoRotationAngle, Vector3.up);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+    }
 
+    public void DisplayCam()
+    {
+        if (!CamDisplay.IsActive())
+        {
+            CamDisplay.gameObject.SetActive(true);
+            Cam.Play();
+        }
+        else
+        {
+            CamDisplay.gameObject.SetActive(false);
+            Cam.Stop();
+        }
     }
 }
